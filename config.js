8a8b02e661a53a50faf61ca25d8694c124606e27
newsifat0-jpg <newsifat0@gmail.com>
2026-08-17 19:06:10 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,31 @@
-window.CINEHUB4_CONFIG={botName:'Cine Hub4',botUsername:'Cinehub4bot',backendUrl:'',ads:{provider:'Adsgram',enabled:true,blockId:'43222',rewardPoints:3,dailyPointAdLimit:10},unlock:{pointsCost:10,hours:15,adUnlockEnabled:true},servers:{server1:true,server2:true,server3:true},language:'bn',admin:{ownerId:'5605543865',adminIds:['7830499211','1904834862']}};
+window.CINEHUB4_CONFIG = {
+  botName: "Cine Hub4",
+  language: "bn",
+
+  unlock: {
+    pointsCost: 10,
+    durationHours: 15,
+    adUnlockEnabled: true
+  },
+
+  ads: {
+    provider: "Adsgram",
+    blockId: "43222",
+    rewardPoints: 3,
+    dailyPointRewardLimit: 10
+  },
+
+  demoAdmin: {
+    enabled: true,
+    ownerId: "5605543865",
+    adminIds: ["7830499211", "1904834862"]
+  },
+
+  servers: {
+    server1: true,
+    server2: true,
+    server3: true
+  },
+
+  backendUrl: ""
+};
