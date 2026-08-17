f41ff27cde3fba71598cc3b75fc3d14f70f8dccd
newsifat0-jpg <newsifat0@gmail.com>
2026-08-17 14:27:19 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
-window.CINEHUB4_CONFIG={botName:"Cine Hub4",botUsername:"Cinehub4bot",backendUrl:"",adsgram:{enabled:true,blockId:"43222"},unlock:{defaultHours:15,defaultCostPoints:10},language:"bn"};
+window.CINEHUB4_CONFIG = {
+  botName: "Cine Hub4",
+  botUsername: "Cinehub4bot",
+  backendUrl: "",
+  adsgram: { enabled: true, blockId: "43222" },
+  unlock: { defaultHours: 15, defaultCostPoints: 10 },
+  defaultLanguage: "bn"
+};
