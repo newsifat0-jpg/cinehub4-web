27e01b4092bb037fb653ab2915d2324960611034
newsifat0-jpg <newsifat0@gmail.com>
2026-08-19 15:39:00 +0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 /* Cine Hub4 frontend configuration */
 window.APP_CONFIG = {
   appName: "Cine Hub4",
-  appId: "cinehub4",
-  apiBaseUrl: "https://script.google.com/macros/s/AKfycbxXSsyQUbGETkYE3KOWWglNLkucBu_LRYp4f7Ioxg1LCP3HSbhbGpBW8aEiMYvNRrlR/exec",
+  apiBaseUrl: "https://script.google.com/macros/s/AKfycby5qWrxgdrW2KUKkSTmo97JZ9QupTRWaXPE7NkS_76E5fkjpBz0eBo9O8a3goQq4aDG/exec",
   botUsername: "@Cinehub4bot",
   unlockHours: 15,
   unlockCost: 5,
