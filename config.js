e76f5516c06ac277b436c6f74ef5d0665e9a5661
newsifat0-jpg <newsifat0@gmail.com>
2026-08-19 10:45:55 +0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,20 +1,27 @@
 /* Cine Hub4 frontend configuration */
-window.APP_CONFIG={
-  appName:"Cine Hub4",
-  botUsername:"@Cinehub4bot",
-  adminDemoId:"5605543865",
-  adminIds:["5605543865"],
-  unlockHours:15,
-  unlockCost:5,
-  adReward:2,
-  dailyAdLimit:20,
-  telegramBotLink:"https://t.me/Cinehub4bot",
-  miniAppName:"Hub4",
-  miniAppLink:"https://t.me/Cinehub4bot/Hub4",
-  telegramChannelLink:"",
-  howToWatchVideo:"",
-  watchTutorialVideo:"",
-  adBlocks:{rewarded:"",interstitial:"",banner:"",bannerAdult:"",task:"",adult:""},
+window.APP_CONFIG = {
+  appName: "Cine Hub4",
+  appId: "cinehub4",
+  apiBaseUrl: "https://script.google.com/macros/s/AKfycbxXSsyQUbGETkYE3KOWWglNLkucBu_LRYp4f7Ioxg1LCP3HSbhbGpBW8aEiMYvNRrlR/exec",
+  botUsername: "@Cinehub4bot",
+  unlockHours: 15,
+  unlockCost: 5,
+  adReward: 2,
+  dailyAdLimit: 20,
+  telegramBotLink: "https://t.me/Cinehub4bot",
+  miniAppName: "Hub4",
+  miniAppLink: "https://t.me/Cinehub4bot/Hub4",
+  telegramChannelLink: "https://t.me/chinehub4",
+  howToWatchVideo: "",
+  watchTutorialVideo: "",
+  adBlocks: { rewarded: "", interstitial: "", banner: "", bannerAdult: "", task: "", adult: "" },
 
-  firebase:{apiKey:"",authDomain:"",projectId:"",storageBucket:"",messagingSenderId:"",appId:""}
+  firebase: {
+    apiKey: "AIzaSyBdoLUyIhL83s7w72eOU_cEdF5aAcqfDdQ",
+    authDomain: "cinehub4-d1773.firebaseapp.com",
+    projectId: "cinehub4-d1773",
+    storageBucket: "cinehub4-d1773.firebasestorage.app",
+    messagingSenderId: "628837201187",
+    appId: "1:628837201187:web:d1b274b8fd3a783710c896"
+  }
 };
